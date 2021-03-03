bc67d54afa12ff93e4eafc99bd748ecd09848d07
msmith <msmith@splunk.com>
2021-03-03 12:15:10 +1100

    change to just checking bitbucket admin

--- fast-check/fast_check.py
+++ fast-check/fast_check.py
@@ -6,16 +6,16 @@ import getpass
 pwd = getpass.getpass(prompt="Enter Password: ", stream=None)
 usr = os.environ["USER"]
 
-request = urllib.request.Request("https://git.splunk.com/rest/api/1.0/admin/cluster")
-str = f"{usr}:{pwd}"
-base64string = base64.standard_b64encode(str.encode("utf-8"))
+request = urllib.request.Request("https://git.splunk.com/rest/api/1.0/logs/rootLogger")
+# str =
+base64string = base64.standard_b64encode(f"{usr}:{pwd}".encode("utf-8"))
 request.add_header("Authorization", f"Basic {base64string.decode('utf-8')}")
 request.add_header("Content-Type", "application/json")
 
 try:
     result = urllib.request.urlopen(request)
 except urllib.error.HTTPError as e:
-    print(f"You are not a bitbucket sys admin!")
+    print(f"You are not a bitbucket admin!")
     print(e)
 else:
-    print("You are a bitbucket sys admin!")
+    print("You are a bitbucket admin!")
