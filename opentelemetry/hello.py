662fc0537e8058d596bd8550c6ea6a731a718c93
Mit Suthar <msuthar@splunk.com>
2021-05-21 17:23:51 +1000

    feat: upgrade and lock packages.
    Integrated according to the instructions in the latest release here: https://github.com/signalfx/splunk-otel-python

--- opentelemetry/hello.py
+++ opentelemetry/hello.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 logging.basicConfig(level=logging.DEBUG)
-logging.info(f"Trace exporter url: {os.environ.get('SPLK_TRACE_EXPORTER_URL')}")
+logging.info(f"Trace exporter url: {os.environ.get('OTEL_EXPORTER_OTLP_ENDPOINT')}")
 
 @app.route('/')
 def hello_world():
